62a60fc619b610d5b51c410fdd4023704b6e1272
Ryan Blau <ryan.blau@wework.com>
2018-02-12 16:42:12 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,4 +2,4 @@ board = [   ,   ,   ,   ,   ,   ,   ,   ,   ,]
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]}"
   puts "-----------"
-  puts " #{board[3]}"
+  puts " #{board[3]} | #{board[0]}
